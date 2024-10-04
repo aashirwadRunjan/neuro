ffed664f026e78041511969b75ca605d6f3dd825
aashirwadRunjan <aashirwad.ranjan.in@rupeek.com>
2024-10-04 11:55:25 +0530

    changes

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Page() {
   return (
     <div>
       <Navbar />
-      <h1>Welcome to the Slot Booking System</h1>
+      <h1>Welcome to the Slot Booking Systems</h1>
       
     </div>
   );
